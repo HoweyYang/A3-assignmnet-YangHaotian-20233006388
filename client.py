fd6bec895546b7216cd19a5d10db2314f496604c
HoweyYang <hy271@students.waikato.ac.nz>
2025-04-29 01:47:17 +0800

    update(not yet!
    soon

--- client.py
+++ client.py
@@ -15,7 +15,7 @@ def recvn(sock, n):
     """
     data = b''
     while len(data) < n:
-        packet = sock.recieve(n - len(data))
+        packet = sock.recv(n - len(data))
         if not packet:
             #if empty bytes, peer closed
             return None
@@ -33,5 +33,66 @@ if __name__ == '__main__':
     if not os.path.isfile(args.file):
         print(f"File{args.file} does not exist")
         sys.exit(1)
+        
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        #create a TCP/IP socket
+        sock.connect((args.host, args.port))
+        #connect to the server
+        
+        with open(args.file, 'r') as f:
+            #open the file in binary mode
+            for line in f:
+                lien = line.strip()
+                #strip whitespace
+                if not line:
+                    continue
+                #skip empty lines
+                parts = line.split(' ', 2)
+                operation = parts[0] #operation:PUT/READ/GET
+                
+                #protocol establishment
+                if operation == 'PUT':
+                    if len(parts) < 3:
+                        print(f"invalid PUT entry: {line}")
+                        continue
+                    # extract the key and value from the parts
+                    key, value = parts[1]. parts[2]
+                    if len(key) + 1 + len(value) > 970:
+                        print(f"entry too long:{line}")
+                        continue
+                    #construct the payload of PUT
+                    payload = f"P{key}{value}"
+                elif operation == 'READ':
+                    if len(parts) < 2:
+                        print(f"invalid READ entry: {line}")
+                        continue
+                    # extract the key from the parts
+                    key = parts[1]
+                    if len(key) > 970:
+                        print(f"entry too long:{line}")
+                        continue
+                    #construct the payload of READ
+                    payload = f"R{key}"
+                elif operation == 'GET':
+                    if len(parts) < 2:
+                        print(f"invalid GET entry: {line}")
+                        continue
+                    # extract the key from the parts
+                    key = parts[1]
+                    if len(key) > 970:
+                        print(f"entry too long, skipping:{line}")
+                        continue
+                    #construct the payload of GET
+                    payload = f"G{key}"
+                else:
+                    print(f"unkonwn opperation, skipping:{line}")
+                    continue
+                    
+                    
+                
+            
+                    
+                    
+                
     
         
