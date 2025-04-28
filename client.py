8e541ea45948c92047bc0d86f68fe7c24f88ffbf
HoweyYang <hy271@students.waikato.ac.nz>
2025-04-29 01:29:46 +0800

    update
    update!(far from completion

--- client.py
+++ client.py
@@ -5,7 +5,33 @@ import os       # for file existence check
 import sys      # for sys.exit
 
 def recvn(sock, n):
-    return 
+    """
+    Helper to recieve exactly n bytes or return None if EOF.
+    Same logic as server's recvn
+    Args:sock (socket.socket): The socket object from which to receive data.
+    n (int): The number of bytes to receive.
+    Returns: bytes: The received data of length n if successful, None otherwise.
+    
+    """
+    data = b''
+    while len(data) < n:
+        packet = sock.recieve(n - len(data))
+        if not packet:
+            #if empty bytes, peer closed
+            return None
+        data += packet
+    return data
 if __name__ == '__main__':
-
-  
+    parser = argparse.ArgumentParser(description = 'Tuple Space Client')
+    parser.add_argument('host', help = 'Server hostname or Ip address')
+    parser.add_argument('port', type = int, help = 'Server port number')
+    parser.add_argument('file', help = 'Path to the request file')
+    #parse command-line arguments
+    args = parser.parse_args()
+    
+    #check if file exists
+    if not os.path.isfile(args.file):
+        print(f"File{args.file} does not exist")
+        sys.exit(1)
+    
+        
