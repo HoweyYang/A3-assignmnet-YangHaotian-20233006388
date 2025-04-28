58617ed482c8a51abf6913667381ef0cc5a586a7
HoweyYang <hy271@students.waikato.ac.nz>
2025-04-29 01:11:57 +0800

    format of client
    format

--- client.py
+++ client.py
@@ -1,2 +1,11 @@
 #client.py
-import socket
+import socket   # for TCP/IP sockets
+import argparse # for command-line parsing
+import os       # for file existence check
+import sys      # for sys.exit
+
+def recvn(sock, n):
+    return 
+if __name__ == '__main__':
+
+  
